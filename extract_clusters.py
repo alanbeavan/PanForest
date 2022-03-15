5962d2ac57128e5ea8426226d4e50799707f6cbe
alanbeavan <Alan.Beavan@nottingham.ac.uk>
2022-03-15 16:00:49 +0000

    added extract_clusters.py

---
+++ extract_clusters.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3.6
+"""Cluster a network and write the clusters to a file."""
+
+import sys
+import argparse
+import os
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms.community import louvain_communities
+from networkx.algorithms.community import greedy_modularity_communities
+
+def get_args():
+    """Get user arguments."""
+
+def get_args():
+    """Get settings from user arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type = str,
+                        help = "Input network file", dest = "infile")
+    parser.add_argument("-e", "--edge-type", type = str,
+                        help = "Interaction type (pp, nn). \
+                                Default = null (both included)",
+                        default = False, dest = "edge_type")
+    parser.add_argument("-m", "--method", type = str, dest = "method",
+                        help = "Method to cluster graph (modularity (greedy),\
+                                 louvain (iterative)), default = modularity",
+                        default = "modularity")
+    parser.add_argument("-o", "--output", type = str, default = "clusters",
+                        help = "Output directory, default = clusters/",
+                        dest = "outdir")
+    args = parser.parse_args()
+    if None in [args.infile]:
+        parser.print_help(sys.stderr)
+        sys.exit(0)
+    return [args.infile, args.edge_type, args.method, args.outdir]
+
+
+def main():
+    """
+    Read in edge table
+    Cluster graph
+    Write clusters to a file
+    """
+    infile, edge_type, method, outdir = get_args()
+    edges = pd.read_csv(infile, header = [0])
+    #edges = edges.head(100)
+    if edge_type:
+        edges = edges[edges["InteractionType"] == edge_type]
+    network = nx.from_pandas_edgelist(edges, source = "Source",
+                                      target = "Target",
+                                      edge_attr = ["InteractionType", "Weight"],
+                                      create_using=nx.DiGraph())
+    if method == "louvain":
+        clusters = louvain_communities(network, weight = "Weight")
+    else:
+        clusters = greedy_modularity_communities(network, weight = "Weight")
+
+    os.mkdir(outdir)
+    for i in range(len(clusters)):
+        if not os.path.exists(outdir + "/cluster_" + str(i)):
+            os.mkdir(outdir + "/cluster_" + str(i))
+        genes = list(clusters[i])
+        with open(outdir + "/cluster_" + str(i) + "/genes_in_cluster",
+                  "w", encoding = "utf8") as out:
+            out.write("\n".join(genes))
+        subset = edges[edges['Target'].isin(genes)]
+        subset = subset[subset['Source'].isin(genes)]
+        subset.to_csv(outdir + "/cluster_" + str(i) + "/cluster.csv",
+                      index = False)
+
+    #nx.draw(network, with_labels=True)
+    #plt.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
