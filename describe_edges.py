1588b290f8f902e868600f2bd7aab03c249caec1
alanbeavan <Alan.Beavan@nottingham.ac.uk>
2022-05-27 15:17:37 +0100

    added describe_edges.py and describe_nodes.py

---
+++ describe_edges.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3.6
+"""Make an edge table with some extra stats."""
+
+import argparse
+import glob
+import sys
+import pandas as pd
+import rf_module as rf
+
+def get_args():
+    """Get user arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--matrix", dest = "matrix_file",
+                        type = str, help = "Matrix file")
+    parser.add_argument("-n", "--network", dest = "network_file",
+                        type = str, help = "network file")
+    parser.add_argument("-o", "--output", dest = "output_file",
+                        type = str, help = "Output table file")
+    args = parser.parse_args()
+    if None in [args.matrix_file, args.network_file, args.output_file]:
+        parser.print_help(sys.stderr)
+        sys.exit(0)
+    return [args.matrix_file, args.network_file, args.output_file]
+
+
+def main():
+    """
+    Write the interactions present in all clusters with extra information.
+    each gene in the presence/asbsence of the other.
+    end result might be a table w/
+    source,target,interaction_type,strength,P(B),P(B|A)P(B|!A),P(A),P(A|B),P(A|!B)P(A+B).
+    """
+    #for now just do one cluster
+    matrix_file, network_file, output_file = get_args()
+    matrix = pd.read_csv(matrix_file, header = 0,
+                         index_col = [0,1,2], dtype = str)
+    matrix = rf.preprocess_df(matrix, 0, 0, 0)
+    new_rows = []
+    for row in matrix.index:
+        new_rows.append(row.split(",")[0])
+    matrix.index = new_rows
+
+    interactions = rf.get_file_data(network_file)[1:]
+
+    table_lines = ["Source(A),Target(B),interaction_type,weight,P(B),P(B|A),P(B|!A),P(A),P(A|B),P(A|!B),P(A+B)"]
+    matrix = matrix.transpose()
+    n_genome = matrix.shape[0]
+    for interaction in interactions:
+        print(interaction)
+        target, source, interaction_type, weight = interaction.split(",")
+        b_freq = "%.2f"%(len(matrix.loc[matrix[source] == 1])/n_genome)
+        a_freq = "%.2f"%(len(matrix.loc[matrix[target] == 1])/n_genome)
+        test_b1 = matrix.loc[matrix[source] == 1]
+        test_b0 = matrix.loc[matrix[source] == 0]
+        conditional_b_freq = str('%2f'%(sum(test_b1[target])/test_b1.shape[0]))
+        neg_b_freq = str('%2f'%(sum(test_b0[target])/test_b0.shape[0]))
+        test_a1 = matrix.loc[matrix[target] == 1]
+        test_a0 = matrix.loc[matrix[target] == 0]
+        conditional_a_freq = str('%2f'%(sum(test_a1[source])/test_a1.shape[0]))
+        neg_a_freq = str('%2f'%(sum(test_a0[source])/test_a0.shape[0]))
+        p_both = str('%2f'%(sum(test_a1[source])/matrix.shape[0]))
+        table_lines.append(",".join([source,target,interaction_type,weight,
+                                     b_freq,conditional_b_freq,neg_b_freq,
+                                     a_freq,conditional_a_freq,neg_a_freq,p_both]))
+    with open(output_file, "w", encoding = "utf8") as out:
+        out.write("\n".join(table_lines))
+
+
+if __name__ == "__main__":
+    main()
---
+++ describe_nodes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3.6
+"""Make an edge table with some extra stats."""
+
+import argparse
+import glob
+import sys
+import pandas as pd
+import rf_module as rf
+
+def get_args():
+    """Get user arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--performance", dest = "performance",
+                        type = str, help = "Performance table")
+    parser.add_argument("-d", "--dtable", dest = "d_table",
+                        type = str, help = "D table")
+    parser.add_argument("-o", "--output", dest = "output_file",
+                        type = str, help = "Output table file")
+    args = parser.parse_args()
+    if None in [args.performance, args.d_table, args.output_file]:
+        parser.print_help(sys.stderr)
+        sys.exit(0)
+    return [args.performance, args.d_table, args.output_file]
+
+
+def main():
+    """
+    Write the interactions present in all clusters with extra information.
+    each gene in the presence/asbsence of the other.
+    end result might be a table w/
+    source,target,interaction_type,strength,P(B),P(B|A)P(B|!A),P(A),P(A|B),P(A|!B)P(A+B).
+    """
+    #for now just do one cluster
+    performance, d_table, output_file = get_args()
+    d_stats = {}
+    for line in rf.get_file_data(d_table)[1:]:
+        fields = line.split("\t")
+        d_stats[fields[0]] = fields[1]
+    performance_lines = rf.get_file_data(performance)[1:]
+    performance_lines = [line.replace("\"", "") for line in performance_lines]
+    table_lines = ["NodeID,Annotation,Wordy Annotation,count,TPte,FPte,FNte,TNte,Ete,Ate,P1te,P0te,Pte,R1te,R0te,Rte,F1te,F0te,Fte,TPtr,FPtr,FNtr,TNtr,Etr,Atr,P1tr,P0tr,Ptr,R1tr,R0tr,Rtr,F1tr,F0tr,Ftr,D statistic"]
+    for line in performance_lines:
+        node_id = line.split(",")[0]
+        try:
+            table_lines.append(line + "," + d_stats[node_id])
+            print(line + "," + d_stats[node_id])
+        except:
+            continue
+
+    with open(output_file, "w", encoding = "utf8") as out:
+        out.write("\n".join(table_lines))
+
+
+if __name__ == "__main__":
+    main()
