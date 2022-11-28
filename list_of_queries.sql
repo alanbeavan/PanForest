8280bcb60581dfef42801d1852327e188d2b6478
alanbeavan <Alan.Beavan@nottingham.ac.uk>
2022-11-28 12:08:32 +0000

    added some sql queries

--- list_of_queries.sql
+++ list_of_queries.sql
@@ -178,8 +178,7 @@ AS SELECT Source_B AS Source,
         AND F1StatisticClass0Test >= 0.9
         AND DStatistic > 0)
     GROUP BY Source_B
-    ORDER BY Edges_emerging DESC
-limit 300;
+    ORDER BY Edges_emerging DESC;
 
 .once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/most_predicting_positive.csv
 SELECT NodeID,
@@ -205,8 +204,7 @@ SELECT Target_A AS Target,
         AND F1StatisticClass0Test >= 0.9
         AND DStatistic > 0)
     GROUP BY Target_A
-    ORDER BY Edges_targeting DESC
-    limit 300;
+    ORDER BY Edges_targeting DESC;
 .once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/most_predicted_positive.csv
 SELECT NodeID,
     Annotation,
@@ -231,8 +229,7 @@ CREATE VIEW MostTargettedNeg AS
         AND F1StatisticClass0Test >= 0.9
         AND DStatistic > 0)
     GROUP BY Source_B
-    ORDER BY Edges_emerging DESC
-    limit 300;
+    ORDER BY Edges_emerging DESC;
 .once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/most_predicting_negative.csv
 
 SELECT NodeID,
@@ -258,8 +255,7 @@ CREATE VIEW MostTargetingNeg AS
         AND F1StatisticClass0Test >= 0.9
         AND DStatistic > 0)
     GROUP BY Target_A
-    ORDER BY Edges_targeting DESC
-    limit 300;
+    ORDER BY Edges_targeting DESC;
 .once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/most_predicted_negative.csv
 
 SELECT NodeID,
@@ -270,3 +266,102 @@ SELECT NodeID,
 FROM MostTargetingNeg M INNER JOIN Nodes N ON M.Target = N.NodeID;
 
 
+--Find the perfectly predicted genes
+/*
+Create VIEW with +ve edges emerging and targeting and -ve edges emerging and targeting and inner join with the nodes table to get everything in the nodes table plus the number of edges emerging etc.
+Take those with perfect accuracy and those without and write to a table
+*/
+CREATE VIEW TargetedPos
+AS SELECT Source_B AS Source,
+        COUNT(*) AS PosEdgesEmerging
+    FROM Edges
+    WHERE InteractionType = 'pp'
+    AND Target_A IN (
+        SELECT NodeID
+        FROM Nodes
+        WHERE ErrorRateTest <= 0.1
+        AND F1StatisticClass1Test >= 0.9
+        AND F1StatisticClass0Test >= 0.9
+        AND DStatistic > 0)
+    GROUP BY Source_B;
+
+CREATE VIEW TargetingPos
+AS SELECT Target_A AS Target,
+        COUNT(*) AS PosEdgesTargeting
+    FROM Edges
+    WHERE InteractionType = 'pp'
+    AND Target_A IN (
+        SELECT NodeID
+        FROM Nodes
+        WHERE ErrorRateTest <= 0.1
+        AND F1StatisticClass1Test >= 0.9
+        AND F1StatisticClass0Test >= 0.9
+        AND DStatistic > 0)
+    GROUP BY Target_A;
+
+CREATE VIEW TargetedNeg
+AS SELECT Source_B AS Source,
+        COUNT(*) AS NegEdgesEmerging
+    FROM Edges
+    WHERE InteractionType = 'nn'
+    AND Target_A IN (
+        SELECT NodeID
+        FROM Nodes
+        WHERE ErrorRateTest <= 0.1
+        AND F1StatisticClass1Test >= 0.9
+        AND F1StatisticClass0Test >= 0.9
+        AND DStatistic > 0)
+    GROUP BY Source_B;
+
+CREATE VIEW TargetingNeg
+AS SELECT Target_A AS Target,
+        COUNT(*) AS NegEdgesTargeting
+    FROM Edges
+    WHERE InteractionType = 'nn'
+    AND Target_A IN (
+        SELECT NodeID
+        FROM Nodes
+        WHERE ErrorRateTest <= 0.1
+        AND F1StatisticClass1Test >= 0.9
+        AND F1StatisticClass0Test >= 0.9
+        AND DStatistic > 0)
+    GROUP BY Target_A;
+
+CREATE TABLE NodesExpanded0
+AS SELECT * 
+FROM Nodes N LEFT JOIN TargetedPos T0 ON T0.Source = N.NodeID;
+ALTER TABLE NodesExpanded0 DROP COLUMN Source;
+
+CREATE TABLE NodesExpanded1
+AS SELECT *
+FROM NodesExpanded0 N LEFT JOIN TargetingPos T1 ON T1.Target = N.NodeID;
+ALTER TABLE NodesExpanded1 DROP COLUMN Target;
+
+CREATE TABLE NodesExpanded2
+AS SELECT *
+FROM NodesExpanded1 N LEFT JOIN TargetedNeg T2 ON T2.Source = N.NodeID;
+ALTER TABLE NodesExpanded2 DROP COLUMN Source;
+
+CREATE TABLE NodesExpanded3
+AS SELECT *
+FROM NodesExpanded2 N LEFT JOIN TargetingNeg T3 ON T3.Target = N.NodeID;
+ALTER TABLE NodesExpanded3 DROP COLUMN Target;
+
+.header ON
+.once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/even_fuller_node_table.csv
+SELECT * FROM NodesExpanded3;
+
+
+.header ON
+.once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/perfectly_predicted_genes.csv
+select * FROM NodesExpanded3 WHERE ErrorRateTest = 0 AND DStatistic > 0;
+
+--Find the genes that are not perfectly predicted (but do pass the D test)
+.header ON
+.once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/not_perfectly_predicted_genes.csv
+select * FROM NodesExpanded3 WHERE ErrorRateTest > 0 AND DStatistic > 0;
+
+--get the worst predicted 304 genes (304 is the number of perfectly predicted genes - i guess rank by average F1 score?!?! - alternatively by worst F1 score???
+.header ON
+.once /Users/ab17362/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Post_doc_notts/sqlite_practice/worst_performing_genes.csv
+SELECT * FROM NodesExpanded3 ORDER BY F1StatisticAverageTest LIMIT 304;
